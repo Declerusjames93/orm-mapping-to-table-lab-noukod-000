54c8b849673f3ac168b4043c0e2ecf22a94026f7
James Declerus <james.declerus@esih.edu>
2019-06-04 15:48:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,11 @@ class Student
     end
 
   def self.drop_table
-
+    sql = <<-SQL
+          DROP TABLE students
+        SQL
+    
+         DB[:conn].execute(sql) 
   end
   def save
       sql = <<-SQL
