81cd5e270ca1f5b4964928e6bf471c7d74154594
James Declerus <james.declerus@esih.edu>
2019-06-04 15:57:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
         DB[:conn].execute(sql, self.name, self.album)
 
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM ")[0][0]
-      end 
+      end
       end
     def self.create(name:, grade:)
         student = Student.new(name, grade)
