6c7f977f6ead81cb9ecce8631ce1a096fb869f6f
James Declerus <james.declerus@esih.edu>
2019-06-04 15:54:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,12 +35,20 @@ class Student
         VALUES (?, ?)
       SQL
 
-      DB[:conn].execute(sql, self.name, self.album)
-
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      DB[:conn].execute(sql, self.name, self.grade)
 
     end
+    def save
+        sql = <<-SQL
+          INSERT INTO students (name, grade)
+          VALUES (?, ?)
+        SQL
 
+        DB[:conn].execute(sql, self.name, self.album)
+
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM ")[0][0]
+
+      end
     def self.create(name:, grade:)
         student = Student.new(name, grade)
         student.save
